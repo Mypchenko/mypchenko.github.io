e231eb8f896c4d9111121981cbb6457f8cd5d4d9
Tim <42820605+TimIrwing@users.noreply.github.com>
2019-03-14 14:17:24 +0300

    Fixed elementHeight()

--- test/scroll-blocks/js/main.js
+++ test/scroll-blocks/js/main.js
@@ -1,13 +1,15 @@
-document.body.style.paddingTop = `5px`;
+document.body.onload = () => {
+  document.body.style.paddingTop = `21px`;
 
-const elements = document.getElementsByClassName(`scroll-element`);
-for (const entry of elements) {
-  entry.style = `padding: 2em 0;`;
-}
-const breakpoints = breakpointsFromArr(elements);
+  const elements = document.getElementsByClassName(`scroll-element`);
+  for (const entry of elements) {
+    entry.style.padding = `1rem 0`;
+  }
 
-const display = document.getElementById(`scroll-display`);
-display.style = `display: flex;
+  const breakpoints = breakpointsFromArr(elements);
+
+  const display = document.getElementById(`scroll-display`);
+  display.style = `display: flex;
                  transform: skewX(-45deg);
                  position: fixed;
                  top: -1px;
@@ -15,58 +17,61 @@ display.style = `display: flex;
                  background: gray;
                  padding: 0.25rem 6px 0.25rem 0;`;
 
-for (let i = -1; i < elements.length; ++i) {
-  const displayNode = document.createElement(`div`);
+  for (let i = -1; i < elements.length; ++i) {
+    const displayNode = document.createElement(`div`);
 
-  displayNode.style = `height: 30px;
+    displayNode.style = `height: 30px;
                    width: 90px;
                    background: #4b4b4b;
                    margin: 0 6px`;
 
-  if (i === -1) displayNode.style.background = `#e49400`;
-  display.appendChild(displayNode);
-}
+    if (i === -1) displayNode.style.background = `#e49400`;
+    display.appendChild(displayNode);
+  }
+  animateScroll();
 
-animateScroll();
+  // window.addEventListener(`scroll`, animateScroll());
 
-function animateScroll() {
-  window.requestAnimationFrame(() => {
-    const displayNodes = display.children;
-    const scroll = pageYOffset + innerHeight;
-    for (let i = 0; i < elements.length; ++i) {
-      if (scroll >= breakpoints[i]) {
-        displayNodes[i + 1].style.backgroundColor = `#e49400`;
-      } else {
-        displayNodes[i + 1].style.backgroundColor = `#4b4b4b`;
+  function animateScroll() {
+    window.requestAnimationFrame(() => {
+      const displayNodes = Array.from(display.children);
+      displayNodes.shift(1);
+      const scroll = pageYOffset + innerHeight;
+      for (let i = 0; i < elements.length; ++i) {
+        if (scroll + 5 > breakpoints[i]) {
+          displayNodes[i].style.backgroundColor = `#e49400`;
+        } else {
+          displayNodes[i].style.backgroundColor = `#4b4b4b`;
+        }
       }
+      animateScroll();
+    });
+  }
+
+  function elementHeight(element) {
+    return parseFloat(element.clientHeight);
+  }
+
+  function breakpointsFromArr(arr) {
+    const result = [];
+    let sum = smallestOffset(arr);
+
+    for (const entry of arr) {
+      const height = elementHeight(entry);
+      sum += height;
+      result.push(sum);
     }
-    animateScroll();
-  });
-}
 
-function elementHeight(element) {
-  return parseFloat(element.clientHeight);
-}
-
-function breakpointsFromArr(arr) {
-  const result = [];
-  let sum = smallestOffset(arr);
-
-  for (const entry of arr) {
-    const height = elementHeight(entry);
-    sum += height;
-    result.push(sum);
+    return result;
   }
 
-  return result;
-}
+  function smallestOffset(arr) {
+    let tmp = Infinity;
 
-function smallestOffset(arr) {
-  let tmp = Infinity;
+    for (const entry of arr) {
+      if (entry.offsetTop < tmp) tmp = entry.offsetTop;
+    }
 
-  for (const entry of arr) {
-    if (entry.offsetTop < tmp) tmp = entry.offsetTop;
+    return tmp;
   }
-
-  return tmp;
-}
+};
