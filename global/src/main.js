53b5de6202d30e1aea753ec471c112d9f2726d37
Tim <timirwing@gmail.com>
2019-04-13 19:17:37 +0300

    [global] Add mainMenu animation and pageReturnArea

--- global/src/main.js
+++ global/src/main.js
@@ -1,11 +1,30 @@
 document.body.onload = () => {
   const page = document.getElementsByClassName(`pageContainer`)[0];
   const mainMenuBtn = document.getElementsByClassName(`mainMenuBtn`)[0];
-  const mainMenu = document.getElementsByClassName(`mainMenu`)[0];
+  const mainMenuElements = Array.from(
+    document.getElementsByClassName(`mainMenuElement`)
+  );
+  const pageReturnArea = document.getElementsByClassName(`pageReturnArea`)[0];
 
-  mainMenuBtn.addEventListener(`click`, (e) => {
+  for (let i = 0; i < mainMenuElements.length; ++i) {
+    mainMenuElements[i].style.transitionDelay = `${i * 0.05}s`;
+  }
+
+  pageReturnArea.addEventListener(`click`, mainMenuViewToggle);
+  mainMenuBtn.addEventListener(`click`, mainMenuViewToggle);
+
+  function mainMenuViewToggle(e) {
     page.classList.toggle(`pageContainer_zoomedOut`);
-    mainMenu.classList.toggle(`mainMenu_visible`);
+    pageReturnArea.classList.toggle(`inactive`);
+
+    const transitionDelayCf = ~e.path.indexOf(mainMenuBtn) ? 0.05 : 0;
+    for (let i = 0; i < mainMenuElements.length; ++i) {
+      mainMenuElements[i].style.transitionDelay = `${i * transitionDelayCf}s`;
+    }
+    mainMenuElements.forEach((element) => {
+      element.classList.toggle(`mainMenuElement_appear`);
+    });
+
     e.stopPropagation;
-  });
+  }
 };
